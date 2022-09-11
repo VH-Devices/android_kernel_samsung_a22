7bae0df10d6fad51b0a71b02d0841d7dddfa55fd
Hakalle <hakalle@proton.me>
2022-09-11 00:49:19 -0300

    CI: Write own build script for kernel
    Signed-off-by: Hakalle <hakalle@proton.me>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,15 +1,52 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CC=$(pwd)/../PLATFORM/prebuilts/clang/host/linux-x86/clang-r383902/bin/clang
-export CLANG_TRIPLE=aarch64-linux-gnu-
-export ARCH=arm64
-export ANDROID_MAJOR_VERSION=r
+# Written by Hakalle (Velosh) <hakalle@proton.me>.
 
-export KCFLAGS=-w
-export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+# Clone GCC & Proton Clang.
+[[ -d "$(pwd)/gcc/" ]] || git clone https://github.com/VH-Devices/toolchains -b gcc-10.3.0 gcc --depth 1 >> /dev/null 2> /dev/null
+[[ -d "$(pwd)/clang/" ]] || git clone https://github.com/kdrag0n/proton-clang clang --depth 1 >> /dev/null 2> /dev/null
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a22_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+# Export KBUILD_BUILD_{USER,HOST} flags.
+export KBUILD_BUILD_USER="$(uname -n)"
+export KBUILD_BUILD_HOST="$(uname -n)"
 
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+# Export ARCH/SUBARCH flags.
+export ARCH="arm64"
+export SUBARCH="arm64"
+
+# Export CCACHE
+export CCACHE_EXEC="$(which ccache)"
+export CCACHE="${CCACHE_EXEC}"
+export CCACHE_COMPRESS="1"
+export USE_CCACHE="1"
+$CCACHE -M 50G
+
+# Export toolchain/clang/llvm flags
+export CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-buildroot-linux-gnu-"
+export CLANG_TRIPLE="aarch64-linux-gnu-"
+export CC="$(pwd)/clang/bin/clang"
+
+# Export if/else outdir var
+export WITH_OUTDIR=true
+
+# Clear the console
+clear
+
+# Remove out dir folder and clean the source
+if [ "${WITH_OUTDIR}" == true ]; then
+   if [ -d "$(pwd)/a22" ]; then
+      rm -rf a22
+   fi
+fi
+
+# Build time
+if [ "${WITH_OUTDIR}" == true ]; then
+   if [ ! -d "$(pwd)/a22" ]; then
+      mkdir a22
+   fi
+fi
+
+if [ "${WITH_OUTDIR}" == true ]; then
+   "${CCACHE}" make O="$(pwd)/a22" a22_defconfig
+   "${CCACHE}" make -j`nproc` O="$(pwd)/a22"
+fi
